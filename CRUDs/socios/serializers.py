889ada0ee63e108f32a52b83e4383def3f9a55d8
Carlos Souza <Carlos.Souza@sprint.com>
2020-10-28 15:25:33 -0300

    adicionando serializer nos foren keys

--- CRUDs/socios/serializers.py
+++ CRUDs/socios/serializers.py
@@ -8,4 +8,4 @@ class SociosSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id','matricula', 'nome', 'data_de_nascimento', 'RG', 'data_emissao', 'local_emissao', 
         'orgao_expedidor', 'CPF', 'titulo_de_Eleitor', 'PIS_PASEP', 'NIT', 'nome_da_Mae', 'nome_do_Pai', 
         'endereco', 'numero', 'complemento', 'UF', 'cidade', 'telefone', 'email', 'data_de_admissao', 
-        'data_de_demissao', 'situacao', 'foto']
+        'data_de_demissao', 'situacao', 'foto', 'perfil']
