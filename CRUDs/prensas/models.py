d540b0a568ff7b86f088a8388862075da37db7b6
Bruno Caboclo dos Santos Ribeiro <bcaboclo@aquarius.cpqd.com.br>
2021-01-06 22:32:59 -0300

    Models em ordem alfabetica e CEP no Sócio

--- CRUDs/prensas/models.py
+++ CRUDs/prensas/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Prensas(models.Model):
     id = models.AutoField(primary_key=True)
-    numero = models.IntegerField(verbose_name='Numero', unique=True, blank=False)
     descricao = models.CharField(verbose_name='Descrição', max_length=100)
     detalhes_tecnicos = models.CharField(verbose_name='Detalhes Técnicos', max_length=100)
+    numero = models.IntegerField(verbose_name='Numero', unique=True, blank=False)
 
     def __int__(self):
         return self.numero + ' - ' + self.descricao
--- CRUDs/socios/serializers.py
+++ CRUDs/socios/serializers.py
@@ -7,5 +7,5 @@ class SociosSerializer(serializers.HyperlinkedModelSerializer):
         model = Socios
         fields = ['id','matricula', 'nome', 'data_de_nascimento', 'RG', 'data_emissao', 'local_emissao', 
         'orgao_expedidor', 'CPF', 'titulo_de_Eleitor', 'PIS_PASEP', 'NIT', 'nome_da_Mae', 'nome_do_Pai', 
-        'endereco', 'numero', 'complemento', 'UF', 'cidade', 'telefone', 'email', 'data_de_admissao', 
+        'endereco', 'numero', 'complemento', 'CEP', 'UF', 'cidade', 'telefone', 'email', 'data_de_admissao', 
         'data_de_demissao', 'situacao', 'foto', 'perfil', 'senha']
