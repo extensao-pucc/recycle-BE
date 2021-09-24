12a96c8dea12e474fb7687aab81eb47d9a55e244
Bruno Caboclo dos Santos Ribeiro <bcaboclo@cpqd.com.br>
2021-09-23 22:21:23 -0300

    Black true colunas tabela parametros e null true fornecedor do lote

--- CRUDs/lote/models.py
+++ CRUDs/lote/models.py
@@ -6,11 +6,12 @@ from CRUDs.fornecedores.models import Fornecedores
 class Lote(models.Model):
     num_lote = models.IntegerField(verbose_name='Numero do Lote', primary_key=True)
     finalizado = models.DateTimeField(verbose_name='Finalizado', blank=False)
-    fornecedor = models.ForeignKey(Fornecedores, on_delete=models.DO_NOTHING, verbose_name='Fornecedores', blank=True)
+    fornecedor = models.ForeignKey(Fornecedores, on_delete=models.DO_NOTHING, verbose_name='Fornecedores', blank=True, null=True)
     iniciado = models.DateTimeField(verbose_name='Iniciado', blank=False)
     observacao = models.CharField(verbose_name='Observação', max_length=200)
     socio = models.ForeignKey(Socios, on_delete=models.DO_NOTHING, verbose_name='Socios')
     tempo_total = models.IntegerField(verbose_name='Tempo Total', blank=False)
+    # nao_processado = models.DecimalField(max_digits=20, decimal_places=2, verbose_name='Não processado')
 
     def __str__(self):
         return str(self.num_lote)
--- CRUDs/parametros/models.py
+++ CRUDs/parametros/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Parametros(models.Model):
     id = models.AutoField(primary_key=True)
-    numero_proxima_NFE = models.CharField(verbose_name='Numero Próxima NFE', max_length=100)
-    numero_proxima_NFS = models.CharField(verbose_name='Numero Próxima NFS', max_length=100)
-    prensa = models.CharField(verbose_name='Prensa', max_length=100, blank=False)
-    remanufatura = models.CharField(verbose_name='Remanufatura', max_length=100, blank=False)
-    triagem = models.CharField(verbose_name='Triagem', max_length=100, blank=False)
-    unidade_de_medida = models.CharField(verbose_name='Unidade de medida', max_length=6)
+    numero_proxima_NFE = models.CharField(verbose_name='Numero Próxima NFE', max_length=100, blank=True)
+    numero_proxima_NFS = models.CharField(verbose_name='Numero Próxima NFS', max_length=100, blank=True)
+    prensa = models.CharField(verbose_name='Prensa', max_length=100, blank=True)
+    remanufatura = models.CharField(verbose_name='Remanufatura', max_length=100, blank=True)
+    triagem = models.CharField(verbose_name='Triagem', max_length=100, blank=True)
+    unidade_de_medida = models.CharField(verbose_name='Unidade de medida', max_length=6, blank=True)
 
     class Meta:
         verbose_name_plural = "Parâmetros"
