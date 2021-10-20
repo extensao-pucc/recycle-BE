01a5fe6625d2efe05e4098d14c4d747c831c266f
Bruno Caboclo dos Santos Ribeiro <bcaboclo@cpqd.com.br>
2021-10-19 22:23:51 -0300

    incluido id no serializer do contas

--- financeiro/contas/serializers.py
+++ financeiro/contas/serializers.py
@@ -5,4 +5,4 @@ from .models import Contas
 class ContasSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Contas
-        fields = ['descricao', 'data', 'tipo', 'valor', 'situacao']
+        fields = ['id', 'descricao', 'data', 'tipo', 'valor', 'situacao']
